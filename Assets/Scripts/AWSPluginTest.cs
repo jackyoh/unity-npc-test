d04ce688b8d9c5eca8c6e3eef6e2abbbaba98df3
jackyoh <jack@is-land.com.tw>
2023-05-22 10:17:22 +0800

    Test aws plugin

--- Assets/Scripts/AWSPluginTest.cs
+++ Assets/Scripts/AWSPluginTest.cs
@@ -15,9 +15,68 @@ using Amazon.S3;
 using Amazon.S3.Model;
 using Amazon.S3.Transfer;
 
-public class AWSPluginTest : MonoBehaviour {
+using System;
+using System.Threading.Tasks;
+using System.Net;
+using TMPro;
 
-    public void Start() {
-        Debug.Log("RUN AWS PLUGIN TEST ..................");
+public class AWSPluginTest : MonoBehaviour {
+    private IAmazonCognitoIdentityProvider cognitoService;
+    public TextMeshProUGUI textComponent;
+
+    public async void Start() {
+        cognitoService = new AmazonCognitoIdentityProviderClient(
+            new AnonymousAWSCredentials(), RegionEndpoint.USEast1);
+        
+        /*var userAttrs = new AttributeType {
+            Name = "email",
+            Value = "jack@is-land.com.tw"
+        };
+        var userAttrsList = new List<AttributeType>();
+        userAttrsList.Add(userAttrs);
+
+        var signUpRequest = new SignUpRequest {
+            UserAttributes = userAttrsList,
+            Username = "user100",
+            ClientId = "7bdudnl6iogq0cthqumnne8bai",
+            Password = "9775630345"
+        };
+        var response = await cognitoService.SignUpAsync(signUpRequest);
+        Debug.Log(response);
+
+        if (response.HttpStatusCode == HttpStatusCode.OK) {
+            Debug.Log("The user100 was created");
+        }*/
+
+        /*string username = "user100";
+        string code = "114492";
+        var confirmSignUpRequest = new ConfirmSignUpRequest {
+            ClientId = "7bdudnl6iogq0cthqumnne8bai",
+            ConfirmationCode = code,
+            Username = username
+        };
+
+        var response = await cognitoService.ConfirmSignUpAsync(confirmSignUpRequest);
+        if (response.HttpStatusCode == HttpStatusCode.OK) {
+            Debug.Log(username + " was confirmed.");
+        }*/
+
+        String username = "user100";
+        String password = "9775630345";
+
+        var authParameters = new Dictionary<string, string>();
+        authParameters.Add("USERNAME", username);
+        authParameters.Add("PASSWORD", password);
+
+        var authRequest = new InitiateAuthRequest {
+            ClientId = "7bdudnl6iogq0cthqumnne8bai",
+            AuthParameters = authParameters,
+            AuthFlow = AuthFlowType.USER_PASSWORD_AUTH,
+        };
+        InitiateAuthResponse response = await cognitoService.InitiateAuthAsync(authRequest);
+        var authResult = response.AuthenticationResult;
+        // var idToken = authResult.IdToken;
+        // textComponent.text = idToken;
+        textComponent.text = authResult.IdToken;
     }
 }
