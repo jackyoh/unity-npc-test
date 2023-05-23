c0e36a5c59f6eeb55f5a67de9afb2c23c0457b74
jackyoh <jack@is-land.com.tw>
2023-05-23 11:28:38 +0800

    Modify third party file

--- Assets/Scripts/AWSPluginTest.cs
+++ Assets/Scripts/AWSPluginTest.cs
@@ -6,15 +6,14 @@ using Amazon;
 using Amazon.Runtime;
 using Amazon.CognitoIdentity;
 using Amazon.CognitoIdentity.Model;
-//using Amazon.CognitoIdentityProvider;
-//using Amazon.CognitoIdentityProvider.Model;
+using Amazon.CognitoIdentityProvider;
+using Amazon.CognitoIdentityProvider.Model;
 
 using Amazon.SecurityToken;
 using Amazon.SecurityToken.Model;
-
 using Amazon.S3;
 using Amazon.S3.Model;
-//using Amazon.S3.Transfer;
+using Amazon.S3.Transfer;
 
 using System;
 using System.Threading.Tasks;
@@ -22,14 +21,15 @@ using System.Net;
 using TMPro;
 
 public class AWSPluginTest : MonoBehaviour {
-    //private IAmazonCognitoIdentityProvider cognitoService;
+    private IAmazonCognitoIdentityProvider cognitoService;
     public TextMeshProUGUI textComponent;
 
-    public void Start() {
-        UnityInitializer.AttachToGameObject(this.gameObject);
+    public async void Start() {
+        // aws-sdk-unity.zip test
+        /*UnityInitializer.AttachToGameObject(this.gameObject);
         string IdentityPoolId = "us-east-1:176365b4-d93d-4589-a8a2-68f41ed6a31d";
         CognitoAWSCredentials _credentials = new CognitoAWSCredentials(IdentityPoolId, RegionEndpoint.USEast1);
-        var s3Client = new AmazonS3Client(_credentials, RegionEndpoint.USEast1);
+        var s3Client = new AmazonS3Client(_credentials, RegionEndpoint.USEast1);*/
 
         /*s3Client.ListBucketsAsync(new ListBucketsRequest(), (responseObject) => {
             if (responseObject.Exception == null) {
@@ -41,10 +41,9 @@ public class AWSPluginTest : MonoBehaviour {
             }
         });*/
 
-        var request = new ListObjectsRequest() {
+        /*var request = new ListObjectsRequest() {
             BucketName = "amplify-unitytest-dev-164133-deployment"
         };
-        
         s3Client.ListObjectsAsync(request, (responseObject) => {
             responseObject.Response.S3Objects.ForEach((o) => {
                 if (responseObject.Exception == null) {
@@ -53,10 +52,12 @@ public class AWSPluginTest : MonoBehaviour {
                     });
                 }
             });
-        });
-
-        /*cognitoService = new AmazonCognitoIdentityProviderClient(
-            new AnonymousAWSCredentials(), RegionEndpoint.USEast1);*/
+        });*/
+        
+        //------------------------------------------------------------------
+        // Get third-part test
+        cognitoService = new AmazonCognitoIdentityProviderClient(
+            new AnonymousAWSCredentials(), RegionEndpoint.USEast1);
         
         /*var userAttrs = new AttributeType {
             Name = "email",
@@ -68,7 +69,7 @@ public class AWSPluginTest : MonoBehaviour {
         var signUpRequest = new SignUpRequest {
             UserAttributes = userAttrsList,
             Username = "user100",
-            ClientId = "7bdudnl6iogq0cthqumnne8bai",
+            ClientId = "76dq8d06b7aio2v74trg8qn0uq",
             Password = "9775630345"
         };
         var response = await cognitoService.SignUpAsync(signUpRequest);
@@ -79,9 +80,9 @@ public class AWSPluginTest : MonoBehaviour {
         }*/
 
         /*string username = "user100";
-        string code = "114492";
+        string code = "629958";
         var confirmSignUpRequest = new ConfirmSignUpRequest {
-            ClientId = "7bdudnl6iogq0cthqumnne8bai",
+            ClientId = "76dq8d06b7aio2v74trg8qn0uq",
             ConfirmationCode = code,
             Username = username
         };
@@ -91,20 +92,21 @@ public class AWSPluginTest : MonoBehaviour {
             Debug.Log(username + " was confirmed.");
         }*/
 
-        /*String username = "user100";
+        String username = "user100";
         String password = "9775630345";
 
         var authParameters = new Dictionary<string, string>();
         authParameters.Add("USERNAME", username);
         authParameters.Add("PASSWORD", password);
 
-        var authRequest = new InitiateAuthRequest {
-            ClientId = "7bdudnl6iogq0cthqumnne8bai",
+        var authRequest = new InitiateAuthRequest{
+            ClientId = "76dq8d06b7aio2v74trg8qn0uq",
             AuthParameters = authParameters,
             AuthFlow = AuthFlowType.USER_PASSWORD_AUTH,
         };
+
         InitiateAuthResponse response = await cognitoService.InitiateAuthAsync(authRequest);
         var authResult = response.AuthenticationResult;
-        textComponent.text = authResult.IdToken;*/
+        textComponent.text = authResult.IdToken;
     }
 }
